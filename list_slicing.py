f4b289658d6c7830931658b2105a7fd9a22eeaab
Prachi Nayakal <121372605+prachinayakal@users.noreply.github.com>
2024-10-29 08:53:53 +0530

    Add files via upload

---
+++ list_slicing.py
@@ -0,0 +1,5 @@
+# List Slicing
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 33, 34, 56, 43, 67]
+sliced_number = num[-8:-2] 
+print("Original list number: ", num) 
+print("Sliced list number : ", sliced_number)
